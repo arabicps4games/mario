908eb6c477704f5e1ea994ada42730738c0bab74
arabicps4games <80811378+arabicps4games@users.noreply.github.com>
2022-10-23 00:22:16 +0100

    Update game.js

--- js/game.js
+++ js/game.js
@@ -106,7 +106,7 @@ function handleInput(dt) {
   } else {
     player.noRun();
   }
-  if (input.isDown('UP')) {
+  if (input.isDown('BUTTON_X')) {
     player.jump();
   } else {
     //we need this to handle the timing for how long you hold it
